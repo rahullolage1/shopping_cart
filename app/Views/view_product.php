33f402ace9caa4f2378a146f097635ce870afd0a
Pravin Windows <pravindot17@gmail.com>
2022-02-28 22:32:21 +0530

    initial commit

--- app/Views/view_product.php
+++ app/Views/view_product.php
@@ -1,6 +1,6 @@
 <div class="container text-center"><br>
   <a class="btn btn-primary" href="<?php echo base_url() ?>/product" role="button">Back</a>
-    <div class="row my-auto">
+    <div class="row">
       <div class="col-sm-12">
         <h3><?php echo $result['name'] ?></h3>
         <p>₹<?= $result['price'] ?></p>
